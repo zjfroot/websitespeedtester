dc87b4daeba518b4d0e5bfd941a3d607c7ce7568
Jifeng Zhang <zjfroot@gmail.com>
2015-07-06 17:51:52 +0200

    phantomjs scripts

---
+++ loadspeed.js
@@ -0,0 +1,30 @@
+var page = require('webpage').create(),
+  system = require('system'),
+  t, address;
+
+page.settings.resourceTimeout = 3000;
+
+if (system.args.length === 1) {
+  console.log('Usage: loadspeed.js <some URL>');
+  phantom.exit();
+}
+
+page.onResourceTimeout = function(e) {
+  console.log(e.errorCode);   // it'll probably be 408 
+  console.log(e.errorString); // it'll probably be 'Network timeout on resource'
+  console.log(e.url);         // the url whose request timed out
+  phantom.exit(1);
+};
+
+t = Date.now();
+address = system.args[1];
+page.open(address, function(status) {
+  if (status !== 'success') {
+    console.log('FAIL to load the address');
+  } else {
+    t = Date.now() - t;
+    console.log('Loading ' + system.args[1]);
+    console.log('Loading time ' + t + ' msec');
+  }
+  phantom.exit();
+});
